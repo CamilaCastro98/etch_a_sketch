cb3abfcc4ac93052b798125cf0aea2ddb48a3217
Camila <camila@MacBook-Pro-de-Adolfo.local>
2023-05-14 20:14:09 -0300

    Fix grid size

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const contenedor = document.createElement('div')
 contenedor.classList.add('contenedor')
 function createGrid() {
     for(let i=0; i<16; i++) {
+        let n = 16
+        let eachDivSize = 600/n
         let horizontalLine = document.createElement('div')
         horizontalLine.classList.add('horizontalLine')
         contenedor.appendChild(horizontalLine)
             for(let i=0 ; i<16 ; i++) {
                 let div=document.createElement('div')
                 div.classList.add('eachDiv')
-                div.textContent = `${i}`
+                div.style.width = eachDivSize + 'px'
+                div.style.height = eachDivSize + 'px'
                 horizontalLine.appendChild(div)
             }
      }   
