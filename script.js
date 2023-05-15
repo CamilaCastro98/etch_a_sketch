6e64c3a0314ddd9db3f68fb86cab9e8b45d47a60
Camila <camila@MacBook-Pro-de-Adolfo.local>
2023-05-14 22:13:51 -0300

    Create hover function

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function createGrid() {
     for(let i=0; i<16; i++) {
         let n = 16
         let eachDivSize = 600/n
-        let horizontalLine = document.createElement('div')
+        var horizontalLine = document.createElement('div')
         horizontalLine.classList.add('horizontalLine')
         contenedor.appendChild(horizontalLine)
             for(let i=0 ; i<16 ; i++) {
-                let div=document.createElement('div')
+                var div=document.createElement('div')
                 div.classList.add('eachDiv')
                 div.style.width = eachDivSize + 'px'
                 div.style.height = eachDivSize + 'px'
@@ -16,5 +16,25 @@ function createGrid() {
             }
      }   
     document.querySelector('body').appendChild(contenedor)
+    getHover()
 }
+
+function paintDivs() {
+    this.style.backgroundColor = 'black'
+}
+
+function getHover() {
+    const gridDivs = document.querySelectorAll('.eachDiv')
+        contenedor.addEventListener('mousedown',()=>{
+            gridDivs.forEach(div =>{
+                div.addEventListener('mousemove',paintDivs)
+            }) 
+        })
+        contenedor.addEventListener('mouseup',()=>{
+            gridDivs.forEach(div => {
+                div.removeEventListener('mousemove',paintDivs)
+            })
+        })
+}
+
 createGrid()
